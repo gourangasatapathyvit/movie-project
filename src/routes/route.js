54975984f66260f5fad9f9c523f6357f276bc96b
Gouranga Satapathy <pulusatapathy@gmail.com>
2023-01-03 01:17:06 +0530

    more info page implementation done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,28 @@
 const express = require('express')
+const _ = require('lodash')
 const routes = express.Router()
 const moviesJson = require('../../public/data/movies.json')
 
 routes.get('/', (req, res) => {
-    // res.send(moviesJson.results)
-    // res.render('index')
+    console.log('triggered at => ' + req.url);
     res.render('index', {
         movieData: moviesJson.results
     })
+})
+
+routes.get('/more-info/:id', (req, res) => {
+    console.log('triggered at => ' + req.url);
+    _.each(moviesJson.results, function (item) {
+        if (req.params.id == item.id) {
+            res.render('moreinfo', {
+                movieResultData: item
+            })
+        }
+    });
+    // console.log(movieResult.id);
+    // res.render('moreinfo', {
+    //     movieResultData: []
+    // })
 })
 
 
