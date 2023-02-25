e41281b8752cc8bb627c2b7fbd57285b8bb70f72
Gouranga Satapathy <pulusatapathy@gmail.com>
2023-02-25 21:54:46 +0530

    event emitter added

--- src/index.js
+++ src/index.js
@@ -51,113 +51,126 @@ hbs.registerHelper("ifCond", function (v1, operator, v2, options) {
   }
 });
 
-mongoose.connect(url, async () => {
-  // let query = { '_id': mongoose.Types.ObjectId('63b97479ac1eede3ef4f0044') }
-  // mongomodels.movieMainPageSchema.create(movieJson)
-  // mongomodels.aboutSchema.create(aboutJson)
+// mongoose.connect(url, async () => {
+//   // let query = { '_id': mongoose.Types.ObjectId('63b97479ac1eede3ef4f0044') }
+//   // mongomodels.movieMainPageSchema.create(movieJson)
+//   // mongomodels.aboutSchema.create(aboutJson)
 
-  /*     let xx =await mongomodels.movieMainPageSchema.find({},"_id")
+//   /*     let xx =await mongomodels.movieMainPageSchema.find({},"_id")
     
-        let x =await mongomodels.movieMainPageSchema.updateOne({
-            "_id": xx[0]._id
-        },
-            {
-                "$push":
-                {
-                    "results":
-                    {
-                        "yearOfRelease": "2032",
-                        "imagePath": "https://lorem",
-                        "title": "lorem test",
-                        "overview": "In ancient Kahndaq, Teth Adam was bestowed the almighty powers of the gods. After using these powers for vengeance, he was imprisoned, becoming Black Adam. Nearly 5,000 years have passed, and Black Adam has gone from man to myth to legend. Now free, his unique form of justice, born out of rage, is challenged by modern-day heroes who form the Justice Society: Hawkman, Dr. Fate, Atom Smasher and Cyclone",
-                        "originalLanguage": "en",
-                        "imdbRating": "8.3",
-                        "originCountry": "United States of America",
-                        "productionHouse": [
-                            "DC Comics"
-                        ],
-                        "itemsInformation": {
-                            "itemType": "Movie"
-                        }
-                    }
-                }
-            }); */
+//         let x =await mongomodels.movieMainPageSchema.updateOne({
+//             "_id": xx[0]._id
+//         },
+//             {
+//                 "$push":
+//                 {
+//                     "results":
+//                     {
+//                         "yearOfRelease": "2032",
+//                         "imagePath": "https://lorem",
+//                         "title": "lorem test",
+//                         "overview": "In ancient Kahndaq, Teth Adam was bestowed the almighty powers of the gods. After using these powers for vengeance, he was imprisoned, becoming Black Adam. Nearly 5,000 years have passed, and Black Adam has gone from man to myth to legend. Now free, his unique form of justice, born out of rage, is challenged by modern-day heroes who form the Justice Society: Hawkman, Dr. Fate, Atom Smasher and Cyclone",
+//                         "originalLanguage": "en",
+//                         "imdbRating": "8.3",
+//                         "originCountry": "United States of America",
+//                         "productionHouse": [
+//                             "DC Comics"
+//                         ],
+//                         "itemsInformation": {
+//                             "itemType": "Movie"
+//                         }
+//                     }
+//                 }
+//             }); */
 
-  /* let upDateresult = await mongomodels.testschema.updateMany(
-        query,
-        {
-            $set: {
-                // "results.$.bookMarkStatus": queryTag,
-                "testschema.res.bookMarkStatus": "holaa",
-                // "bookMarkStatu": "hola"
-            }
-        }
-    ); */
+//   /* let upDateresult = await mongomodels.testschema.updateMany(
+//         query,
+//         {
+//             $set: {
+//                 // "results.$.bookMarkStatus": queryTag,
+//                 "testschema.res.bookMarkStatus": "holaa",
+//                 // "bookMarkStatu": "hola"
+//             }
+//         }
+//     ); */
 
-  /* mongomodels.testschema.create({
-       name:"polo g loea",
-       id:1,
-       isUsed:false,
-       "lora":1234
-   }) */
+//   /* mongomodels.testschema.create({
+//        name:"polo g loea",
+//        id:1,
+//        isUsed:false,
+//        "lora":1234
+//    }) */
 
-  /* let results = await mongomodels.movieMainPageSchema.updateOne(
-        {'results._id':mongoose.Types.ObjectId('63bab79d8ee9f60f78726237')},
-        {$set:{
-            "results.$.title":"hum do Hamare Do"
-        }}
-    ); */
+//   /* let results = await mongomodels.movieMainPageSchema.updateOne(
+//         {'results._id':mongoose.Types.ObjectId('63bab79d8ee9f60f78726237')},
+//         {$set:{
+//             "results.$.title":"hum do Hamare Do"
+//         }}
+//     ); */
 
-  /* let resultsRegex = await mongomodels.movieMainPageSchema.aggregate(
-        [
-            { $unwind: '$results' },
-            {
-                $match: {
-                    // 'results.title':  { $regex: 'the|black', $options: 'i' }
-                    'results.title': { $regex: 'the|black|adam', $options: 'i' }
-                }
-            },
-            {
-                $project: {
-                    _id: 0,
-                    'results._id': 0,
-                    '__v': 0
-                }
-            }
-        ]
-    ); */
+//   /* let resultsRegex = await mongomodels.movieMainPageSchema.aggregate(
+//         [
+//             { $unwind: '$results' },
+//             {
+//                 $match: {
+//                     // 'results.title':  { $regex: 'the|black', $options: 'i' }
+//                     'results.title': { $regex: 'the|black|adam', $options: 'i' }
+//                 }
+//             },
+//             {
+//                 $project: {
+//                     _id: 0,
+//                     'results._id': 0,
+//                     '__v': 0
+//                 }
+//             }
+//         ]
+//     ); */
 
-  /*   let results = await mongomodels.movieMainPageSchema.aggregate(
-          [
-              {
-                  $project: {
-                      _id: 0,  // to supress id
-                      results: {
-                          $filter: {
-                              input: "$results",
-                              as: "result",
-                              cond: { $eq: ["$$result.title", "The Kashmir Files"] }
-                              // cond: { $eq: ["$$result._id", mongoose.Types.ObjectId('63ba4ab0818099c9bf301566')] }
-                              // cond: {$in: [ "$$result.title", ['The Kashmir Files'] ] }
+//   /*   let results = await mongomodels.movieMainPageSchema.aggregate(
+//           [
+//               {
+//                   $project: {
+//                       _id: 0,  // to supress id
+//                       results: {
+//                           $filter: {
+//                               input: "$results",
+//                               as: "result",
+//                               cond: { $eq: ["$$result.title", "The Kashmir Files"] }
+//                               // cond: { $eq: ["$$result._id", mongoose.Types.ObjectId('63ba4ab0818099c9bf301566')] }
+//                               // cond: {$in: [ "$$result.title", ['The Kashmir Files'] ] }
   
-                              // cond: {
-                              //     $regexMatch: {
-                              //         input: "$$result.title",
-                              //         regex: "Kashmir"
-                              //     }
-                              // }
+//                               // cond: {
+//                               //     $regexMatch: {
+//                               //         input: "$$result.title",
+//                               //         regex: "Kashmir"
+//                               //     }
+//                               // }
   
   
-                          }
-                      }
-                  }
-              }
-          ]
-      ); */
-  // console.log(resultsRegex);
-  console.log("mongo is connected");
+//                           }
+//                       }
+//                   }
+//               }
+//           ]
+//       ); */
+//   // console.log(resultsRegex);
+//   console.log("mongo is connected");
+// });
+
+mongoose.connect(url);
+mongoose.connection.once("open", () => {
+  app.emit("ready");
 });
 
-app.listen(PORT, () => {
-  console.log("Server connected at:", PORT);
+app.on("ready", function () {
+  
+  app.listen(PORT, () => {
+    console.log("Server connected at:", PORT);
+  });
+
 });
+
+// app.listen(PORT, () => {
+//   console.log("Server connected at:", PORT);
+// });
